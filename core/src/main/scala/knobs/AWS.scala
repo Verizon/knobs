783e112659b19fa1da08fbbcb90d525b485c1737
tmperret <timothy.m.perrett@intel.com>
2014-04-07 22:05:22 -0700

    updated the aws config to load up a few more config values

--- core/src/main/scala/knobs/AWS.scala
+++ core/src/main/scala/knobs/AWS.scala
@@ -18,11 +18,13 @@ object aws {
   // using a fixed version to avoid api shift over time.
   private val revision = "2012-01-12"
 
+  private val defaultFormating: String => String = x => "\"" +x+ "\""
+
   def fetch(child: Path, parent: Path = root, version: Path = revision): Task[String] =
     Task(Source.fromInputStream(new URL(s"$parent/$version/$child").openConnection.getInputStream).mkString)
 
-  def convert(field: String, f: String => String = x => "\"" +x+ "\"")(response: String): Task[Config] =
-    Config.parse(s"aws { $field = ${f(response)} }").fold(Task.fail, Task.now)
+  def convert(field: String, section: String = "aws", formatter: String => String = defaultFormating)(response: String): Task[Config] =
+    Config.parse(s"$section { $field = ${formatter(response)} }").fold(Task.fail, Task.now)
 
   def userdata: Task[Config] = fetch("user-data")
     .flatMap(response => Config.parse(response)
@@ -34,14 +36,31 @@ object aws {
   def ami: Task[Config] =
     fetch("meta-data/ami-id").flatMap(convert("ami-id"))
 
-  def securityGroups: Task[Config] = fetch("meta-data/security-groups").flatMap(
-    convert("security-groups", x => "[" + x.split('\n').map(x => "\""+x+"\"").mkString(", ") + "]"  ))
+  def securitygroups: Task[Config] = fetch("meta-data/security-groups").flatMap(
+    convert("security-groups", formatter = x => "[" + x.split('\n').map(x => "\""+x+"\"").mkString(", ") + "]"  ))
 
+  def zone: Task[Config] =
+    fetch("meta-data/placement/availability-zone").flatMap(convert("availability-zone"))
+
+  def localIP: Task[Config] =
+    fetch("meta-data/local-ipv4").flatMap(convert("local-ipv4", section = "network"))
+
+  def publicIP: Task[Config] =
+    fetch("meta-data/public-ipv4").flatMap(convert("public-ipv4", section = "network"))
+
+  /**
+   * Overhead for calling this mofo is high, so its not the kind of
+   * thing you want to call anymore than once, but its value is cached internally
+   * within the Config object that this is composed with.
+   */
   lazy val config: Task[Config] =
     (for {
-      a <- userdata
-      b <- instance
-      c <- ami
-      d <- securityGroups
-    } yield a ++ b ++ c ++ d) or Task.now(Config.empty) // fallback for running someplace other than aws.
+      a <- instance
+      b <- ami
+      c <- zone
+      d <- securitygroups
+      e <- localIP
+      f <- publicIP
+      g <- userdata
+    } yield a ++ b ++ c ++ d ++ e ++ f ++ g) or Task.now(Config.empty) // fallback for running someplace other than aws.
 }
